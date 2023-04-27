6db5356bd928f99246ef562cef5d387689800218
Albert <albert959614@gmail.com>
2023-04-27 18:24:26 +0300

    Spring

---
+++ FirstRestApp/src/main/java/ru/Spring/springcourse/FirstRestApp/repositories/PeopleRepository.java
@@ -0,0 +1,11 @@
+package ru.Spring.springcourse.FirstRestApp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.Spring.springcourse.FirstRestApp.models.Person;
+
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
